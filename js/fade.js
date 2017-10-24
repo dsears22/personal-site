08756f2d49bcac8f433fa6e9ecf6824241864236
Daniel Sears <daniel@Daniels-MacBook-Pro-2.local>
2017-10-24 10:38:33 -0700

    Making some stuff blue, adding some dummy JQ

--- js/fade.js
+++ js/fade.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $(".fade").hide().fadeIn(1000);
 });
+
+// $(".hello").hover(function(){
+//   $(".container").css("background-color","#FF0000");
+//   },function(){
+//   $(".container").css("background-color","#DDDDDD");
+// });
